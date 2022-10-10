0ec9bfbe62201abd7f730f486e406709d6bd0ccc
NutthapoomXOM <nutthapoom.suwanwitwaj@exxonmobil.com>
2022-10-10 14:42:00 +0700

    fix ItemCard css

--- src/components/ItemCatalog.js
+++ src/components/ItemCatalog.js
@@ -4,76 +4,77 @@ import ItemCard from './ItemCard'
 import Loader from './Loader'
 import '../components/stylesheets/customcss/itemCard.css'
 
+
 function ItemCatalog() {
-    const api = `https://6339ae2d66857f698fba10a2.mockapi.io/dbapi/itemDetails/`
-    const [ items, setItems ] = useState({
-        loading: false,
-        data: null,
-        err: false
+  const api = `https://6339ae2d66857f698fba10a2.mockapi.io/dbapi/itemDetails/`
+  const [items, setItems] = useState({
+    loading: false,
+    data: null,
+    err: false
+  })
+
+  let itemCardContent = null
+
+  useEffect(() => {
+    setItems({
+      loading: true,
+      data: null,
+      err: false
     })
 
-    let itemCardContent = null
-
-    useEffect(() => {
+    axios.get(api)
+      .then(res => {
         setItems({
-          loading: true,
-          data: null,
+          loading: false,
+          data: res.data,
           err: false
         })
-    
-        axios.get(api)
-          .then(res => {
-            setItems({
-              loading: false,
-              data: res.data,
-              err: false
-            })
+      })
+
+      .catch(() => {
+        setItems({
+          loading: false,
+          data: null,
+          err: true
         })
+      })
 
-        .catch(() => {
-          setItems(
-            loading: false,
-            data: null,
-            err: true
-          })
-        })
+  }, [api])
 
-      }, [api])
-
-    if(items.err) {
-      return (
-        <div>ERROR: item data not found</div>
-      )
-    }
-
-      if(items.loading) {
-        return (
-          <div className='#'>
-            <Loader />
-          </div>
-        )
-      }
-
-    if(items.data) {
-      return (
-        <div className='item-card-container em-l-container'>
-          {items.data.map(item, key => 
-            <div key={item.id}>
-            <ItemCard 
-              item={item} 
-            />
-          </div>
-          )}
-        </div>
-      )
-    }
-  
-
+if (items.err) {
   return (
-    <div className='item-card-container em-l-container'>
-      {itemCardContent}
+    <div>ERROR: item data not found</div>
+  )
+}
+
+if (items.loading) {
+  return (
+    <div className='#'>
+      <Loader />
     </div>
   )
+}
+
+if (items.data) {
+  return (
+    <div >
+      {items.data.map((item, key )=>
+        <div className='item-card-container em-l-container' key={item.id}>
+          <ItemCard
+            item={item}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+
+return (
+  <div className='item-card-container em-l-container'>
+    {itemCardContent}
+  </div>
+)
 }
 
 export default ItemCatalog
